e151107db1aa71d9e26820e341301a954e944f5f
Dustin <dburckh@gmail.com>
2022-01-09 09:43:18 -0700

    Use same Hue rotation matrix as other ImageProcessors

--- RenderScriptMigrationSample/app/src/main/java/com/android/example/rsmigration/RenderEffectImageProcessor.kt
+++ RenderScriptMigrationSample/app/src/main/java/com/android/example/rsmigration/RenderEffectImageProcessor.kt
@@ -11,9 +11,9 @@ import kotlin.math.cos
 import kotlin.math.sin
 
 @RequiresApi(Build.VERSION_CODES.S)
-class RenderEffectImageProcessor:ImageProcessor {
+class RenderEffectImageProcessor : ImageProcessor {
     override val name = "RenderEffect"
-    private var params:Params? = null
+    private var params: Params? = null
 
     override fun configureInputAndOutput(inputImage: Bitmap, numberOfOutputImages: Int) {
         params = Params(
@@ -22,12 +22,12 @@ class RenderEffectImageProcessor:ImageProcessor {
         )
     }
 
-    private fun applyEffect(it:Params, renderEffect: RenderEffect, outputIndex: Int):Bitmap {
+    private fun applyEffect(it: Params, renderEffect: RenderEffect, outputIndex: Int): Bitmap {
         it.renderNode.setRenderEffect(renderEffect)
         //This isn't necessary, but cleans up the logs
         it.hardwareBuffers[outputIndex]?.close()
         val renderCanvas = it.renderNode.beginRecording()
-        renderCanvas.drawBitmap(it.bitmap, 0f,0f, null)
+        renderCanvas.drawBitmap(it.bitmap, 0f, 0f, null)
         it.renderNode.endRecording()
         it.hardwareRenderer.createRenderRequest()
             .setWaitForPresent(true)
@@ -36,44 +36,33 @@ class RenderEffectImageProcessor:ImageProcessor {
         val image = it.imageReader.acquireNextImage() ?: throw RuntimeException("No Image")
         val hardwareBuffer = image.hardwareBuffer ?: throw RuntimeException("No HardwareBuffer")
         it.hardwareBuffers[outputIndex] = hardwareBuffer
-        val bitmap = Bitmap.wrapHardwareBuffer(hardwareBuffer, null) ?: throw RuntimeException("Create Bitmap Failed")
+        val bitmap = Bitmap.wrapHardwareBuffer(hardwareBuffer, null)
+            ?: throw RuntimeException("Create Bitmap Failed")
         image.close()
         return bitmap
     }
 
     override fun rotateHue(radian: Float, outputIndex: Int): Bitmap {
         params?.let {
-            //Adapted from
-            //https://stackoverflow.com/questions/4354939/understanding-the-use-of-colormatrix-and-colormatrixcolorfilter-to-modify-a-draw
             val colorMatrix = it.matrices[outputIndex]
             if (radian == 0f) {
                 colorMatrix.reset()
             } else {
-                val array = colorMatrix.array
-                val cosVal = cos(radian.toDouble()).toFloat()
-                val sinVal = sin(radian.toDouble()).toFloat()
-                array[0]=lumR + cosVal * (1 - lumR) + sinVal * -lumR
-                array[1]=lumG + cosVal * -lumG + sinVal * -lumG
-                array[2]=lumB + cosVal * -lumB + sinVal * (1 - lumB)
-                //3=    0f,
-                //4=    0f,
-                array[5]=lumR + cosVal * -lumR + sinVal * 0.143f
-                array[6]=lumG + cosVal * (1 - lumG) + sinVal * 0.140f
-                array[7]=lumB + cosVal * -lumB + sinVal * -0.283f
-                //8    0f,
-                //9    0f,
-                array[10]=lumR + cosVal * -lumR + sinVal * -(1 - lumR)
-                array[11]=lumG + cosVal * -lumG + sinVal * lumG
-                array[12]=lumB + cosVal * (1 - lumB) + sinVal * lumB
-                //13    0f,
-                //14    0f,
-                //15    0f,
-                //16    0f,
-                //17    0f,
-                //18    1f,
-                //19    0f,
+                val mat = colorMatrix.array
+                val cos = cos(radian.toDouble())
+                val sin = sin(radian.toDouble())
+                mat[0] = (.299 + .701 * cos + .168 * sin).toFloat()
+                mat[1] = (.587 - .587 * cos + .330 * sin).toFloat()
+                mat[2] = (.114 - .114 * cos - .497 * sin).toFloat()
+                mat[5] = (.299 - .299 * cos - .328 * sin).toFloat()
+                mat[6] = (.587 + .413 * cos + .035 * sin).toFloat()
+                mat[7] = (.114 - .114 * cos + .292 * sin).toFloat()
+                mat[10] = (.299 - .300 * cos + 1.25 * sin).toFloat()
+                mat[11] = (.587 - .588 * cos - 1.05 * sin).toFloat()
+                mat[12] = (.114 + .886 * cos - .203 * sin).toFloat()
             }
-            val colorFilterEffect = RenderEffect.createColorFilterEffect(ColorMatrixColorFilter(colorMatrix))
+            val colorFilterEffect =
+                RenderEffect.createColorFilterEffect(ColorMatrixColorFilter(colorMatrix))
             return applyEffect(it, colorFilterEffect, outputIndex)
         }
         throw RuntimeException("Not configured!")
@@ -81,8 +70,10 @@ class RenderEffectImageProcessor:ImageProcessor {
 
     override fun blur(radius: Float, outputIndex: Int): Bitmap {
         params?.let {
-            val blurRenderEffect = RenderEffect.createBlurEffect(radius, radius,
-                Shader.TileMode.MIRROR)
+            val blurRenderEffect = RenderEffect.createBlurEffect(
+                radius, radius,
+                Shader.TileMode.MIRROR
+            )
             return applyEffect(it, blurRenderEffect, outputIndex)
         }
         throw RuntimeException("Not configured!")
@@ -90,36 +81,33 @@ class RenderEffectImageProcessor:ImageProcessor {
 
     override fun cleanup() {
         params?.let {
+            params = null
             it.imageReader.close()
             it.renderNode.discardDisplayList()
             it.hardwareRenderer.destroy()
-            params = null
         }
     }
 
-    inner class Params(val bitmap : Bitmap, numberOfOutputImages: Int) {
+    inner class Params(val bitmap: Bitmap, numberOfOutputImages: Int) {
         @SuppressLint("WrongConstant")
-        val imageReader = ImageReader.newInstance(bitmap.width, bitmap.height, PixelFormat.RGBA_8888, numberOfOutputImages,
-            HardwareBuffer.USAGE_GPU_SAMPLED_IMAGE or HardwareBuffer.USAGE_GPU_COLOR_OUTPUT)
+        val imageReader = ImageReader.newInstance(
+            bitmap.width, bitmap.height,
+            PixelFormat.RGBA_8888, numberOfOutputImages,
+            HardwareBuffer.USAGE_GPU_SAMPLED_IMAGE or HardwareBuffer.USAGE_GPU_COLOR_OUTPUT
+        )
         val renderNode = RenderNode("RenderEffect")
         val hardwareBuffers = arrayOfNulls<HardwareBuffer>(numberOfOutputImages)
-        val matrices = Array(numberOfOutputImages) {ColorMatrix()}
+        val matrices = Array(numberOfOutputImages) { ColorMatrix() }
         val hardwareRenderer = HardwareRenderer()
 
         init {
             hardwareRenderer.setSurface(imageReader.surface)
             hardwareRenderer.setContentRoot(renderNode)
-            renderNode.setPosition(0,0,imageReader.width, imageReader.height)
+            renderNode.setPosition(0, 0, imageReader.width, imageReader.height)
             for (matrix in matrices) {
-                val array = matrix.array
-                array[18] = 1f
+                val mat = matrix.array
+                mat[18] = 1f
             }
         }
-    }
-
-    companion object {
-        const val lumR = 0.213f
-        const val lumG = 0.715f
-        const val lumB = 0.072f
     }
 }
