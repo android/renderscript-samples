a6ab2ecce1df75866964cc00092b649fb9916d24
Dustin <dburckh@gmail.com>
2022-01-13 17:20:46 -0700

    Changes from code review

--- RenderScriptMigrationSample/app/src/main/java/com/android/example/rsmigration/RenderEffectImageProcessor.kt
+++ RenderScriptMigrationSample/app/src/main/java/com/android/example/rsmigration/RenderEffectImageProcessor.kt
@@ -24,8 +24,6 @@ class RenderEffectImageProcessor : ImageProcessor {
 
     private fun applyEffect(it: Params, renderEffect: RenderEffect, outputIndex: Int): Bitmap {
         it.renderNode.setRenderEffect(renderEffect)
-        //This isn't necessary, but cleans up the logs
-        it.hardwareBuffers[outputIndex]?.close()
         val renderCanvas = it.renderNode.beginRecording()
         renderCanvas.drawBitmap(it.bitmap, 0f, 0f, null)
         it.renderNode.endRecording()
@@ -35,31 +33,35 @@ class RenderEffectImageProcessor : ImageProcessor {
 
         val image = it.imageReader.acquireNextImage() ?: throw RuntimeException("No Image")
         val hardwareBuffer = image.hardwareBuffer ?: throw RuntimeException("No HardwareBuffer")
-        it.hardwareBuffers[outputIndex] = hardwareBuffer
         val bitmap = Bitmap.wrapHardwareBuffer(hardwareBuffer, null)
             ?: throw RuntimeException("Create Bitmap Failed")
+        hardwareBuffer.close()
         image.close()
         return bitmap
     }
 
     override fun rotateHue(radian: Float, outputIndex: Int): Bitmap {
         params?.let {
-            val colorMatrix = it.matrices[outputIndex]
-            if (radian == 0f) {
-                colorMatrix.reset()
+            val colorMatrix = if (radian == 0f) {
+                ColorMatrix()
             } else {
-                val mat = colorMatrix.array
                 val cos = cos(radian.toDouble())
                 val sin = sin(radian.toDouble())
-                mat[0] = (.299 + .701 * cos + .168 * sin).toFloat()
-                mat[1] = (.587 - .587 * cos + .330 * sin).toFloat()
-                mat[2] = (.114 - .114 * cos - .497 * sin).toFloat()
-                mat[5] = (.299 - .299 * cos - .328 * sin).toFloat()
-                mat[6] = (.587 + .413 * cos + .035 * sin).toFloat()
-                mat[7] = (.114 - .114 * cos + .292 * sin).toFloat()
-                mat[10] = (.299 - .300 * cos + 1.25 * sin).toFloat()
-                mat[11] = (.587 - .588 * cos - 1.05 * sin).toFloat()
-                mat[12] = (.114 + .886 * cos - .203 * sin).toFloat()
+                ColorMatrix(floatArrayOf(
+                    (.299 + .701 * cos + .168 * sin).toFloat(), //0
+                    (.587 - .587 * cos + .330 * sin).toFloat(), //1
+                    (.114 - .114 * cos - .497 * sin).toFloat(), //2
+                    0f, 0f,                                     //3,4
+                    (.299 - .299 * cos - .328 * sin).toFloat(), //5
+                    (.587 + .413 * cos + .035 * sin).toFloat(), //6
+                    (.114 - .114 * cos + .292 * sin).toFloat(), //7
+                    0f, 0f,                                     //8,9
+                    (.299 - .300 * cos + 1.25 * sin).toFloat(), //10
+                    (.587 - .588 * cos - 1.05 * sin).toFloat(), //11
+                    (.114 + .886 * cos - .203 * sin).toFloat(), //12
+                    0f, 0f, 0f, 0f, 0f,                         //13,14,15,16,17
+                    1f, 0f                                      //18,19
+                ))
             }
             val colorFilterEffect =
                 RenderEffect.createColorFilterEffect(ColorMatrixColorFilter(colorMatrix))
@@ -96,12 +98,6 @@ class RenderEffectImageProcessor : ImageProcessor {
             HardwareBuffer.USAGE_GPU_SAMPLED_IMAGE or HardwareBuffer.USAGE_GPU_COLOR_OUTPUT
         )
         val renderNode = RenderNode("RenderEffect")
-        val hardwareBuffers = arrayOfNulls<HardwareBuffer>(numberOfOutputImages)
-        val matrices = Array(numberOfOutputImages) {
-            val colorMatrix = ColorMatrix()
-            colorMatrix.array[18] = 1f
-            colorMatrix
-        }
         val hardwareRenderer = HardwareRenderer()
 
         init {
